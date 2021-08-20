d63b8596f8294976b9fcfd1af5a384d146e1cd6d
JIAHONGZHANG <550820992@qq.com>
2021-08-20 19:58:57 +1000

    Add stylelint for vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,9 @@
 
 <style>
 .form-item {
+  display: block;
+  color: red;
+  font-size: 12px;
   @apply mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0;
 }
 </style>
---
+++ stylelint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['stylelint-config-recommended'],
+  plugins: ['stylelint-order', 'stylelint-config-rational-order/plugin'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen'],
+      },
+    ],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+    'order/properties-order': [],
+    'plugin/rational-order': [
+      true,
+      {
+        'border-in-box-model': false,
+        'empty-line-between-groups': false,
+      },
+    ],
+  },
+};
