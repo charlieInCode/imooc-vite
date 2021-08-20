de9a2f093f6c98644093fa0bbc272a8ee84f9894
JIAHONGZHANG <550820992@qq.com>
2021-08-20 19:24:49 +1000

    Add vscode config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,11 @@
-<script setup>
-import ButtonRepo from '@/components/ButtonRepo.vue';
-</script>
-
 <template>
   <div class="bg-gray-50">
-    <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-      <h2 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-        Ready to dive in?
-        <br />
-        <span class="text-indigo-600">Vite + Vue 3 + Tailwind CSS</span>
-      </h2>
-      <div class="flex mt-8 lg:flex-shrink-0 lg:mt-0">
-        <div class="inline-flex rounded-md shadow">
-          <router-link
-            to="/about"
-            class="
-              inline-flex
-              items-center
-              justify-center
-              px-5
-              py-3
-              text-base
-              font-medium
-              leading-6
-              text-white
-              transition
-              duration-150
-              ease-in-out
-              bg-indigo-600
-              border border-transparent
-              rounded-md
-              hover:bg-indigo-500
-              focus:outline-none
-            "
-            >Next Page</router-link
-          >
-        </div>
-        <ButtonRepo />
-      </div>
-    </div>
+    <input type="text" class="form-item" />
   </div>
 </template>
+
+<style>
+.form-item {
+  @apply mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0;
+}
+</style>
